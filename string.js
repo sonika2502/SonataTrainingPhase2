2d875f655b8dc87779da85fff9e4e40d0c31b961
dorasonik <118761011+dorasonik@users.noreply.github.com>
2024-08-11 20:22:43 +0530

    added all files

---
+++ string.js
@@ -0,0 +1,7 @@
+//Declare two string variables and concatenate them. Display the result.
+function concateString(str1, str2) {
+    console.log(str1 + str2);
+}
+var string1 = "enter string 1";
+var string2 = "enter string 2";
+concateString(string1, string2);
---
+++ string.ts
@@ -0,0 +1,9 @@
+//Declare two string variables and concatenate them. Display the result.
+
+function concateString(str1,str2){
+    console.log(str1+str2);
+}
+
+let string1="enter string 1";
+let string2="enter string 2";
+concateString(string1,string2);
